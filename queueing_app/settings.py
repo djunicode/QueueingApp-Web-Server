555fd864762b07c6b415800e8f746b4994a1927e
Sahil Jajodia <jajodiasahil@gmail.com>
2018-01-10 18:16:09 +0530

    Added all models and modified settings

--- queueing_app/settings.py
+++ queueing_app/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'queues',
+    'rest_framework',
+    'django_mysql',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'queueing_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'queue',
+        'USER': 'abc',
+        'PASSWORD': 'example@123',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -118,3 +125,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# AUTH_USER_MODEL = 'queues.Profile'
